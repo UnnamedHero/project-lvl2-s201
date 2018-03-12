8c0df703bcd84fbfab51518d1341436b6954a801
UnnamedHero <hero.nospam@gmail.com>
2018-03-12 17:46:04 +0300

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,10 +4,20 @@ import gendiff from '../src';
 const fixturePath = '__tests__/__fixtures__';
 
 describe('gendiff', () => {
+  const expected = fs.readFileSync(`${fixturePath}/plain-json-expected.txt`, 'utf8');
   test('with plain json', () => {
-    const expected = fs.readFileSync(`${fixturePath}/plain-json-expected.txt`, 'utf8');
     const beforeFile = `${fixturePath}/before.json`;
-    const afterFile = `${fixturePath}/after.json`;    
+    const afterFile = `${fixturePath}/after.json`;
+    expect(gendiff(beforeFile, afterFile)).toBe(expected);
+  });
+  test('with plain yaml', () => {
+    const beforeFile = `${fixturePath}/before.yaml`;
+    const afterFile = `${fixturePath}/after.yaml`;
+    expect(gendiff(beforeFile, afterFile)).toBe(expected);
+  });
+  test('with plain json amd yaml', () => {
+    const beforeFile = `${fixturePath}/before.json`;
+    const afterFile = `${fixturePath}/after.yaml`;
     expect(gendiff(beforeFile, afterFile)).toBe(expected);
   });
 });
