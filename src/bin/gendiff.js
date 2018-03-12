62b741a8a35b7a2f168b7ec5acecc3f2ba54dbe0
UnnamedHero <hero.nospam@gmail.com>
2018-03-12 15:45:14 +0300

    add calling gendiff library

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
 import { version as gendiffVersion } from '../../package.json';
+import gendiff from '../';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version(gendiffVersion)
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(gendiff(firstConfig, secondConfig));
+  })
   .parse(process.argv);
