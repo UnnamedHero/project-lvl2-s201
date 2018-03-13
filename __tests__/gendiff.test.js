d81202573c050e1829c533e3da61d73e30e5a102
UnnamedHero <hero.nospam@gmail.com>
2018-03-13 10:26:08 +0300

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,20 +4,16 @@ import gendiff from '../src';
 const fixturePath = '__tests__/__fixtures__';
 
 describe('gendiff', () => {
-  const expected = fs.readFileSync(`${fixturePath}/plain-json-expected.txt`, 'utf8');
+  const expected = fs.readFileSync(`${fixturePath}/plain/plain-expected.txt`, 'utf8');
+  const beforeFilePath = `${fixturePath}/plain/before`;
+  const afterFilePath = `${fixturePath}/plain/after`;
   test('with plain json', () => {
-    const beforeFile = `${fixturePath}/before.json`;
-    const afterFile = `${fixturePath}/after.json`;
-    expect(gendiff(beforeFile, afterFile)).toBe(expected);
+    expect(gendiff(`${beforeFilePath}.json`, `${afterFilePath}.json`)).toBe(expected);
   });
   test('with plain yaml', () => {
-    const beforeFile = `${fixturePath}/before.yaml`;
-    const afterFile = `${fixturePath}/after.yaml`;
-    expect(gendiff(beforeFile, afterFile)).toBe(expected);
+    expect(gendiff(`${beforeFilePath}.yaml`, `${afterFilePath}.yaml`)).toBe(expected);
   });
   test('with plain json amd yaml', () => {
-    const beforeFile = `${fixturePath}/before.json`;
-    const afterFile = `${fixturePath}/after.yaml`;
-    expect(gendiff(beforeFile, afterFile)).toBe(expected);
+    expect(gendiff(`${beforeFilePath}.json`, `${afterFilePath}.yaml`)).toBe(expected);
   });
 });
