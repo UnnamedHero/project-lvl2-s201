99f76885e5e9f2a134b7ddc3c60288e41b60f841
UnnamedHero <hero.nospam@gmail.com>
2018-03-13 17:17:01 +0300

    refactor computeDifference

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import _ from 'lodash';
-import configParser from './configParser';
-import fileLoader from './fileLoader';
+import getConfigObject from './configParser';
+
+const isBothObjectsHaveKey = (key, obj1, obj2) => _.has(obj1, key) && _.has(obj2, key);
+
+const isObjectsValuesEqual = (key, obj1, obj2) => obj1[key] === obj2[key];
+
+const isRemovedValue = (key, obj) => !_.has(obj, key);
 
 const computeObjDifference = (before, after) => {
   const keysUnion = _.union(Object.keys(before), Object.keys(after));
   const result = keysUnion.reduce((acc, key) => {
-    const beforeValue = before[key];
-    const afterValue = after[key];
-    const beforeString = `${key}: ${beforeValue}`;
-    const afterString = `${key}: ${afterValue}`;
-    if (beforeValue && afterValue) {
-      return beforeValue === afterValue ? [...acc, `    ${beforeString}`]
+    const beforeString = `${key}: ${before[key]}`;
+    const afterString = `${key}: ${after[key]}`;
+    if (isBothObjectsHaveKey(key, before, after)) {
+      return isObjectsValuesEqual(key, before, after) ? [...acc, `    ${beforeString}`]
         : [...acc, `  - ${beforeString}`, `  + ${afterString}`];
     }
-    return beforeValue ? [...acc, `  - ${beforeString}`] : [...acc, `  + ${afterString}`];
+    if (isRemovedValue(key, after)) {
+      return [...acc, `  - ${beforeString}`];
+    }
+    return [...acc, `  + ${afterString}`];
   }, []);
   return result;
 };
 
 export default (pathToFile1, pathToFile2) => {
-  const beforeConfigObj = configParser(fileLoader(pathToFile1));
-  const afterConfigObj = configParser(fileLoader(pathToFile2));
+  const beforeConfigObj = getConfigObject(pathToFile1);
+  const afterConfigObj = getConfigObject(pathToFile2);
   const difference = computeObjDifference(beforeConfigObj, afterConfigObj);
   return ['{', ...difference, '}'].join('\n');
 };
