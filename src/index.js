f987c67e68b7da24ddb6c1578c0d7b040725491a
UnnamedHero <hero.nospam@gmail.com>
2018-03-14 18:02:39 +0300

    add AST from calculated differences

--- src/index.js
+++ src/index.js
@@ -2,22 +2,41 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import getParser from './configParser';
+import getRender from './astRenders';
 
-const computeObjDifference = (before, after) => {
+// TODO if both values are objects they must not be marked as "changed"
+const getDifferenceAst = (before, after) => {
   const keysUnion = _.union(Object.keys(before), Object.keys(after));
-  const result = keysUnion.map((key) => {
+  const ast = keysUnion.map((key) => {
+    const beforeValue = before[key];
+    const afterValue = after[key];
     const isBothObjectsHaveKey = () => _.has(before, key) && _.has(after, key);
-    const isObjectsValuesEqual = () => before[key] === after[key];
+    const isObjectsValuesEqual = () => {
+      if (!_.isEqual(beforeValue, afterValue)) {
+        return false;
+      }
+      return true;
+    };
     const isRemovedValue = () => !_.has(after, key);
-    const beforeString = `${key}: ${before[key]}`;
-    const afterString = `${key}: ${after[key]}`;
+    const makeAstNode = (state) => {
+      const astNode = {
+        key,
+        state,
+      };
+      if (_.isObject(beforeValue) && _.isObject(afterValue)) {
+        const children = getDifferenceAst(beforeValue, afterValue);
+        return { ...astNode, children };
+      }
+      return { ...astNode, beforeValue, afterValue };
+    };
+    // const beforeString = `${key}: ${before[key]}`;
+    // const afterString = `${key}: ${after[key]}`;
     if (isBothObjectsHaveKey()) {
-      return isObjectsValuesEqual() ? `    ${beforeString}`
-        : `  - ${beforeString}\n  + ${afterString}`;
+      return isObjectsValuesEqual() ? makeAstNode('unchanged') : makeAstNode('changed');
     }
-    return isRemovedValue() ? `  - ${beforeString}` : `  + ${afterString}`;
+    return isRemovedValue() ? makeAstNode('removed') : makeAstNode('added');
   });
-  return result;
+  return ast;
 };
 
 const getConfigObject = (pathToFile) => {
@@ -30,6 +49,8 @@ const getConfigObject = (pathToFile) => {
 export default (pathToFile1, pathToFile2) => {
   const beforeConfigObj = getConfigObject(pathToFile1);
   const afterConfigObj = getConfigObject(pathToFile2);
-  const difference = computeObjDifference(beforeConfigObj, afterConfigObj);
-  return ['{', ...difference, '}'].join('\n');
+  const differenceAst = getDifferenceAst(beforeConfigObj, afterConfigObj);
+  const renderType = 'json-like';
+  const render = getRender(renderType);
+  return render(differenceAst);
 };
