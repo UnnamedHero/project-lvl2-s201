d6c0c359323de182b96f2820a50738ffaa4b5d99
UnnamedHero <hero.nospam@gmail.com>
2018-03-19 01:06:55 +0300

    refactor plain rendering

--- src/renders/plain.js
+++ src/renders/plain.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
 
 const renderValue = (value) => {
-  if (!value) {
-    return '';
-  }
   if (_.isPlainObject(value)) {
     return ' complex value';
   }
   if (_.isArray(value)) {
     const [added, removed] = value;
-    const simpleParse = v => (_.isPlainObject(v) ? 'complex value' : `'${v}'`);
-    return ` From ${simpleParse(removed)} to ${simpleParse(added)}`;
+    const parseChangedVal = v => (_.isPlainObject(v) ? 'complex value' : `'${v}'`);
+    return ` From ${parseChangedVal(removed)} to ${parseChangedVal(added)}`;
   }
   return ` value: '${value}'`;
 };
 
 const buildString = (parents, actionString, value) =>
-  `Property '${parents.join('.')}' ${actionString}${renderValue(value)}`;
+  `Property '${parents.join('.')}' ${actionString}${value ? renderValue(value) : ''}`;
 
 const renderPlain = (ast, parents = []) => {
   const nodeParsers = {
-    nested: (node, propParents) => renderPlain(node.children, propParents),
-    unchanged: () => null,
-    added: (node, propParents) => buildString(propParents, 'was added with', node.value),
-    removed: (node, propParents) => buildString(propParents, 'was removed'),
-    changed: (node, propParents) => buildString(propParents, 'was changed.', [node.value, node.oldValue]),
+    nested: (node, propParents) => renderPlain(node.children, [...propParents, node.key]),
+    added: (node, propParents) => buildString([...propParents, node.key], 'was added with', node.value),
+    removed: (node, propParents) => buildString([...propParents, node.key], 'was removed'),
+    changed: (node, propParents) => buildString([...propParents, node.key], 'was changed.', [node.value, node.oldValue]),
 
   };
-  const diffStrings = ast.map((node) => {
-    const parser = nodeParsers[node.type];
-    return parser(node, [...parents, node.key]);
-  });
-  const filteredItems = diffStrings.filter(v => v);
-  return filteredItems.join('\n');
+  const diffStrings = ast
+    .filter(node => node.type !== 'unchanged')
+    .map((node) => {
+      const parser = nodeParsers[node.type];
+      return parser(node, parents);
+    });
+  return diffStrings
+    .filter(v => v)
+    .join('\n');
 };
 
 export default renderPlain;
